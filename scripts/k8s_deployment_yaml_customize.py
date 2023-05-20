1a7f9e3847f4b42debded427bb3ee4f0f61452f8
Lidor <lidororanim@gmail.com>
2023-05-20 20:14:19 +0300

    deleted unnecessary brackets

--- scripts/k8s_deployment_yaml_customize.py
+++ scripts/k8s_deployment_yaml_customize.py
@@ -5,8 +5,8 @@ import os
 env = os.getenv("APP_ENV")
 bot_image = os.getenv("BOT_IMAGE_NAME")
 worker_image = os.getenv("WORKER_IMAGE_NAME")
-file_to_save = f'{os.getenv("K8S_DEPLOYMENT_FILE")}'
-file_to_open = f'{os.getenv("K8S_YAML_TO_EDIT")}'
+file_to_save = os.getenv("K8S_DEPLOYMENT_FILE")
+file_to_open = os.getenv("K8S_YAML_TO_EDIT")
 
 with open(file_to_open) as file:
     y = yaml.safe_load(file)
